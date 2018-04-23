ea967e724293309cb8ad807277a253231c807ce0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Printing;
+
+namespace PageHeightDemo {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            SimpleLink link = new SimpleLink((DataTemplate)Resources["detail"], 1);
+            link.PageHeader = (DataTemplate)Resources["pageHeader"];
+            link.PageFooter = (DataTemplate)Resources["pageFooter"];
+            link.ShowPrintPreviewDialog(this);
+        }
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Printing
+
+Namespace PageHeightDemo
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim link As New SimpleLink(CType(Resources("detail"), DataTemplate), 1)
+			link.PageHeader = CType(Resources("pageHeader"), DataTemplate)
+			link.PageFooter = CType(Resources("pageFooter"), DataTemplate)
+            link.ShowPrintPreviewDialog(Me)
+        End Sub
+	End Class
+End Namespace
