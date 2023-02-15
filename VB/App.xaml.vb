15dc2e1dd01f5c42cd606fcb885b19a477a2ae96
DevExpressExampleBot <fakeEmail>
2023-02-15 12:53:09 +0400

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace PageHeightDemo
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,21 +1,21 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Windows.Controls
 Imports DevExpress.Xpf.Printing
 
 Namespace PageHeightDemo
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim link As New SimpleLink(DirectCast(Resources("detail"), DataTemplate), 1)
-			link.PageHeaderTemplate = DirectCast(Resources("pageHeader"), DataTemplate)
-			link.PageFooterTemplate = DirectCast(Resources("pageFooter"), DataTemplate)
-			link.ShowPrintPreviewDialog(Me)
-		End Sub
-	End Class
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim link As SimpleLink = New SimpleLink(CType(Resources("detail"), DataTemplate), 1)
+            link.PageHeaderTemplate = CType(Resources("pageHeader"), DataTemplate)
+            link.PageFooterTemplate = CType(Resources("pageFooter"), DataTemplate)
+            link.ShowPrintPreviewDialog(Me)
+        End Sub
+    End Class
 End Namespace
